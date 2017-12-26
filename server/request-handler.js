2046008f310a9ed7047df8e9e4d19e5c5716d976
Daham Drake Wang <drakewang@gmail.com>
2017-12-25 23:37:17 -0800

    Modify server to allow api id and key

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,17 +11,61 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
+var APP_ID = 'chatterbox';
+var APP_KEY = 'abc123';
 
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'access-control-allow-headers': [
+    'content-type',
+    'accept',
+    'X-Parse-Application-Id',
+    'X-Parse-REST-API-Key'
+  ].join(),
 };
 
 var data = {
   results: []
+};
+
+var checkHeader = function(request, response, header) {
+  var appId = request.headers['X-Parse-Application-Id'.toLowerCase()];
+  var appKey = request.headers['X-Parse-REST-API-Key'.toLowerCase()];
+  if (appId !== APP_ID || appKey !== APP_KEY) {
+    response.writeHead(403, header);
+    response.end();
+  }
+};
+
+var handleMessages = function(request, response, header) {
+  header['Content-Type'] = 'application/json';
+  header['X-Parse-Application-Id'] = 'chatterbox';
+  header['X-Parse-REST-API-Key'] = 'abc123';
+
+  if (request.method === 'GET') {
+    checkHeader(request, response, header);
+    response.writeHead(200, header);
+    response.end(JSON.stringify(data));
+  } else if (request.method === 'POST') {
+    checkHeader(request, response, header);
+    request.on('data', function(incoming) {
+      data.results.push(JSON.parse(incoming));
+    });
+    request.on('end', function() {
+      response.writeHead(201, header);
+      response.end(JSON.stringify(data));
+    });
+  } else {
+    response.writeHead(201, header);
+    response.end(JSON.stringify(null));
+  }
+};
+
+var handle404 = function(request, response) {
+  response.writeHead(404, defaultCorsHeaders);
+  response.end();
 };
 
 var routes = {
@@ -30,9 +74,10 @@ var routes = {
 
 var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  if (routes[request.url]) {
-    routes[request.url](request, response);
+  var header = Object.assign({}, defaultCorsHeaders);
+  var currentRoute = routes[request.url];
+  if (currentRoute) {
+    currentRoute(request, response, header);
   } else {
     handle404(request, response);
   }
@@ -41,25 +86,3 @@ var requestHandler = function(request, response) {
 module.exports = {
   requestHandler: requestHandler
 };
-
-function handleMessages(request, response) {
-  if (request.method === 'GET') {
-    response.writeHead(200, defaultCorsHeaders);
-    response.end(JSON.stringify(data));
-  } else if (request.method === 'POST') {
-    request.on('data', function(incoming) {
-      data.results.push(JSON.parse(incoming));
-    });
-    request.on('end', function() {
-      response.writeHead(201, defaultCorsHeaders);
-      response.end(JSON.stringify(data));
-    });
-  } else {
-    console.log('not possible');
-  }
-}
-
-function handle404(request, response) {
-  response.writeHead(404, defaultCorsHeaders);
-  response.end();
-}
